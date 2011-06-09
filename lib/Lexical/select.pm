4d79dad1f68804b6a38359f4c5dc704af07260a7
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-06-10 00:49:39 +0100

    Added explicit restore() method, tests and documentation

--- lib/Lexical/select.pm
+++ lib/Lexical/select.pm
@@ -16,9 +16,16 @@ sub lselect {
   return bless { old_fh => $old_fh }, __PACKAGE__;
 }
 
+sub restore {
+  my $self = shift;
+  return if $self->{_restored};
+  select delete $self->{old_fh};
+  return $self->{_restored} = 1;
+}
+
 sub DESTROY {
   my $self = shift;
-  select $self->{old_fh};
+  $self->restore unless $self->{_restored};
 }
 
 q[select $old_fh];
@@ -42,11 +49,39 @@ q[select $old_fh];
 
 =head1 DESCRIPTION
 
+Changing the currently selected filehandle with C<select> and changing it back to the previously selected filehandle
+can be slightly tedious. Wouldn't it be great to have something handle this boilerplate, especially in lexical scopes.
+
+This is where Lexical::select comes in.
+
+Lexical::select provides the C<lselect> function. As demonstrated in the C<SYNOPSIS>, C<lselect> will change the currently
+selected filehandle to the filehandle of your choice for the duration of the enclosing lexical scope.
+
 =head1 FUNCTIONS
+
+Functions exported by default.
 
 =over
 
 =item C<lselect>
+
+Takes one parameter, a C<filehandle> that will become the currently selected filehandle for the duration of the enclosing scope.
+
+Returns an object, which provides the C<restore> method.
+
+You can then either C<restore> the currently selected filehandle back manually or let the object fall out of
+scope, which automagically restores.
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item C<restore>
+
+Explicitly restores the currently selected filehandle back to the original filehandle. This is called automagically
+when the object is C<DESTROY>ed, for instance when the object goes out of scope.
 
 =back
 
